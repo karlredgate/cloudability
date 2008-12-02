b7d8c4d166cbec0107ea2b22e4b03ccb9f18dfa2
cloudability <cloudability@440c00b4-aac6-11dd-a8c6-677c70d10fe4>
2008-12-02 07:27:56 +0000

    Improved the init file parsing with "AWS" and "INSTANCE" in addition to "HOST" and "KEY"

--- init/init.sh
+++ init/init.sh
@@ -1,8 +1,14 @@
 # Replace this with a list of initialization commands for a
-# newly started server using "HOST" instead of the hostname
-# and "KEY" instead of the path to your ".pem" SSH key file.
+# newly started server using placeholders instead of values:
+# - "INSTANCE" is the AWS instance ID of the new instance
+# - "HOST" is the public DNS hostname of the new instance
+# - "KEY" is the path to the key file
+# - "AWS" is the path to the AWS command
 #
 # For example:
 #
+# AWS attvol vol-a1b2c3d4 -i INSTANCE -d /dev/sdb
 # scp -i KEY ~/perl/env.pl root@HOST:cloudability/perl/
 # ssh -i KEY root@HOST cloudability/bin/cloudserver -restart
+# ssh -i KEY root@HOST cloudability/bin/newuser admin:admin
+# ssh -i KEY root@HOST /etc/init.d/mysql restart
